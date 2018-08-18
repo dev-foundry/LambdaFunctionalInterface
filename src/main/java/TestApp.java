dadbea823c6452c8f6b16ad51c96c123c5541679
dnowak <devfoundry01@gmail.com>
2018-08-18 23:07:20 +0200

    added list example

--- src/main/java/TestApp.java
+++ src/main/java/TestApp.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class TestApp {
 
     public static void main(String[] args) {
@@ -12,6 +15,10 @@ public class TestApp {
         Factor factor = a -> a * a;
 
         System.out.println(factor.calculate(x));
+
+        List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5);
+
+        integerList.forEach(element -> System.out.println(element));
 
         NothingSpecial nothingSpecial = TestApp::printMessage;
 
